7185e16679e6ca3f5be9278782c1a4627fe9d08c
Aleksandr Zidyganov <alex@csscoder.pro>
2022-04-10 15:50:52 +0500

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Russia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+sudo pacman -S --noconfirm gdm firefox vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+sudo reboot
